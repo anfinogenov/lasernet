a1a48e372fd5c8667fe88c97d37db961a8c3eae3
Maxim Anfinogenov <m.anfinogenov0@gmail.com>
2017-05-28 18:52:17 +0300

    integrity check added
    Signed-off-by: Maxim Anfinogenov <m.anfinogenov0@gmail.com>

--- laser_loopback/laser_loopback.ino
+++ laser_loopback/laser_loopback.ino
@@ -1,26 +1,28 @@
-const int sleepmks = 250; //150 is ok
-//const int sleepms = 200;
+const int sleepmks = 250; //250 is ok
+const int sleepms = 200;
 const int laserPin = 13;
 const int photoresistorPin = 0;
 const int error = 5;
-int laser_on = 1007;
-//int laser_off = 997;
+int laser_on = 1000;
 
 void setup() {
   pinMode(laserPin, OUTPUT);
   digitalWrite(laserPin, HIGH);
   Serial.begin(115200);
+  delay(100);
+  laser_on = analogRead(photoresistorPin);
 }
 
 char send_receive_byte (char symbol) {
   unsigned char byte_read = 0;
   for (int i = 128; i >= 1; i /= 2) {
-    if (symbol & i)
-      digitalWrite(laserPin, HIGH);
-    else digitalWrite(laserPin, LOW);
+    if (symbol & i) digitalWrite(laserPin, HIGH);
+    else  digitalWrite(laserPin, LOW);
+    
     delayMicroseconds(sleepmks);
     //delay(sleepms);
-    if ((analogRead(photoresistorPin)) > (laser_on - error))
+
+    if ((analogRead(photoresistorPin)) > (laser_on - error))\
       byte_read |= i;
   }
   digitalWrite(laserPin, LOW);
--- serial_file_64bytes_loop.py
+++ serial_file_64bytes_loop.py
@@ -4,6 +4,8 @@ import serial
 import sys
 import os
 import time
+import hashlib
+import math
 
 arduino = serial.Serial(sys.argv[1], 115200, timeout=1)
 
@@ -15,16 +17,54 @@ data = file_to_send.read(64)
 
 arduino.write(data)
 arduino.read(64)
-arduino.write(data) # idk why, but first 128 bytes are missing???
+arduino.write(data)  # idk why, but first 128 bytes are missing???
 arduino.read(64)
 
+packets_sent = 0
+mode = ""
+if len(sys.argv) >= 4:
+    mode = sys.argv[3]
+
+bytes_in_second = 339.7
+if mode == "md5":
+    bytes_in_second = bytes_in_second * 64 / 80  # 16 additional bytes for every 64 bytes of data
+else:
+    bytes_in_second = bytes_in_second * 64 / 66  # 2 additional bytes
+
 print(f"File size: {send_file_size} bytes")
-print(f"Predicted transfer time: {round(send_file_size/339.7, 3)} seconds")
+print(f"Connection speed: {round(bytes_in_second/1024, 3)} KB/s")
+print(f"""Predicted transfer time: 
+    {math.floor(send_file_size/bytes_in_second/60)}m {math.ceil(send_file_size/bytes_in_second)%60}s""")
+
 t0 = time.time()
 while data != b'':
-    print(f"\r{file_to_send.tell()*100 // send_file_size}%", end='')
-    arduino.write(data)
-    received_file.write(arduino.read(64))
-    data = file_to_send.read(64)
-print(f"\nReal transfer time: {round(time.time() - t0, 3)} seconds")
+    print(f"\r{file_to_send.tell()*100 // send_file_size}% - {packets_sent} packets...", end='')
+    
+    check_sum = b''
+    if mode == "md5":
+        check_sum = bytes.fromhex(hashlib.md5(data).hexdigest())
+    else:
+        check_sum = ((sum(data)+0xFDDF) % 0xFFFF).to_bytes(2, byteorder='little', signed=False)
 
+    arduino.write(data)
+    received_data = arduino.read(64)
+    
+    arduino.write(check_sum)
+    received_sum = arduino.read(len(check_sum))
+
+    if mode == "md5":
+        if bytes.fromhex(hashlib.md5(received_data).hexdigest()) != received_sum:
+            print(" MD5 hash error! Retrying...", end=' ')
+            print(f"Expected sum: {hashlib.md5(received_data).hexdigest()}, received: {received_sum.hex()}")
+            continue
+    else:
+        if received_sum != ((sum(received_data)+0xFDDF) % 0xFFFF).to_bytes(2, byteorder='little', signed=False):
+            print(" control sum error! Retrying...")
+            continue
+
+    packets_sent += 1
+    received_file.write(received_data)
+    data = file_to_send.read(64)
+
+print(f"\nReal transfer time: {round(time.time() - t0, 3)} seconds")
+print(f"64B packets sent: {packets_sent}")
